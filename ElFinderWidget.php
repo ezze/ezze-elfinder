9ff23356c12dfe4892ba6ca4822f1175cc3ecef2
Dmitriy Pushkov <ezze@ezze.org>
2013-06-22 13:32:38 +0400

    Readme has been updated.

--- ElFinderWidget.php
+++ ElFinderWidget.php
@@ -6,7 +6,7 @@ Yii::import("ext.ezzeelfinder.ElFinderConnectorAction");
  * A widget to integrate ElFinder uploader.
  *
  * @author Dmitriy Pushkov <ezze@ezze.org>
- * @version 0.0.4
+ * @version 0.0.5
  */
 class ElFinderWidget extends CWidget
 {
